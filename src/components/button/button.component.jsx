d4e27b4ef06f4d6155cd80059038a703a38c1cdb
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2024-10-31 07:16:54 +0200

    add footer ui

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -3,24 +3,36 @@ import { Link } from 'react-router-dom'
 import DynamicIcon from '../dynamicIcon.component';
 import { ReactComponent as ArrowRight } from '../../assets/arrow-right.svg';
 import './button.style.scss'
-const Button = ({ label, icon: Icon, href, className, color, hoverColor, ...props }) => {
+const Button = ({ label, icon: Icon, href, className, color, hoverColor, type = "button", ...props }) => {
     const [isHovered, setIsHovered] = useState(false);
 
     const handleMouseEnter = () => setIsHovered(true);
     const handleMouseLeave = () => setIsHovered(false)
     return (
-        className.includes("primary") ?
-            <Link to={href} className={`button ${className}`} style={{ backgroundColor: isHovered ? `${hoverColor}` : `${color}` }} onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave} {...props}>
-                <span className="button-label pr-4">{label}</span>
-                <ArrowRight className='inline' />
-            </Link>
-            :
-            <Link to={href} className={`button ${className}`} style={{ color: isHovered ? `${hoverColor}` : `${color}`, backgroundColor: '', }} onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave} {...props}>
-                {<DynamicIcon iconName={Icon} className={`link-icon inline w-[16px] mr-2 pb-1 ${Icon}`} />}
-                <span className="button-label ">{label}</span>
-            </Link>
+        href ? (
+            className.includes("primary") ?
+                <Link to={href} className={`button ${className}`} style={{ backgroundColor: isHovered ? `${hoverColor}` : `${color}` }} onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave} {...props}>
+                    <span className="button-label pr-4">{label}</span>
+                    <ArrowRight className='inline' />
+                </Link>
+                :
+                <Link to={href} className={`button ${className}`} style={{ color: isHovered ? `${hoverColor}` : `${color}`, backgroundColor: '', }} onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave} {...props}>
+                    {<DynamicIcon iconName={Icon} className={`link-icon inline w-[16px] mr-2 pb-1 ${Icon}`} />}
+                    <span className="button-label ">{label}</span>
+                </Link>) : (<button
+                    type={type}
+                    className={`button ${className}`}
+                    style={{ backgroundColor: isHovered ? `${hoverColor}` : `${color}` }}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    {...props}
+                >
+                    {Icon && <DynamicIcon iconName={Icon} className={`link-icon inline w-[16px] mr-2 pb-1 ${Icon}`} />}
+                    <span className="button-label">{label}</span>
+                    <ArrowRight className='inline' />
+                </button>)
     )
 };
 
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import './footer.style.scss';
-import { ReactComponent as ArrowSelect} from '../../assets/arrow-select.svg';
-import { ReactComponent as ArrowRight} from '../../assets/arrow-right.svg';
+import { ReactComponent as ArrowRight } from '../../assets/arrow-right.svg';
+import Button from '../button/button.component';
+
 
 const Footer = ({ data }) => {
 
@@ -28,82 +29,85 @@ const Footer = ({ data }) => {
 
     return (
         <div className="footer">
-            {/* Intro Section */}
-            <div className="footer-intro">
-                <h2>{intro.title}</h2>
-                <div
-                    className="footer-intro-text"
-                    dangerouslySetInnerHTML={{ __html: intro.text }}
-                />
-                <Link to={intro.link.href} className="footer-link">
-                    {intro.link.label}
-                </Link>
-            </div>
-            <ArrowSelect />
-            <ArrowRight fill='orange' />
+            <div className="footer-content">
 
-            {/* Form Section */}
-            <div className="footer-form">
-                <h3>{form.title}</h3>
-                <form name={form.name} onSubmit={handleSubmit}>
-                    {form.fields.map((field, index) => {
-                        if (field.type === 'text') {
-                            return (
-                                <input
-                                    key={index}
-                                    type="text"
-                                    name={field.name}
-                                    placeholder={field.name}
-                                    className={`form-input ${field.size === 'half' ? 'half-width' : 'full-width'}`}
-                                    onChange={(e) => handleChange(field.name, e.target.value)}
-                                />
-                            );
-                        } else if (field.type === 'textarea') {
-                            return (
-                                <textarea
-                                    key={index}
-                                    name={field.name}
-                                    placeholder={field.name}
-                                    className={`form-textarea ${field.size === 'full' ? 'full-width' : ''}`}
-                                    onChange={(e) => handleChange(field.name, e.target.value)}
-                                />
-                            );
-                        } else if (field.type === 'options') {
-                            return field.layout === 'select' ? (
-                                <select
-                                    key={index}
-                                    name={field.name}
-                                    className={`form-select ${field.size === 'half' ? 'half-width' : 'full-width'}`}
-                                    onChange={(e) => handleChange(field.name, e.target.value)}
-                                >
-                                    {field.options.map((option, i) => (
-                                        <option key={i} value={option.value}>
-                                            {option.label}
-                                        </option>
-                                    ))}
-                                </select>
-                            ) : (
-                                <div key={index} className={`form-buttons ${field.size === 'full' ? 'full-width' : 'half-width'}`}>
-                                    {field.options.map((option, i) => (
-                                        <label key={i} className="form-button-option">
-                                            <input
-                                                type="radio"
-                                                name={field.name}
-                                                value={option.value}
-                                                onChange={(e) => handleChange(field.name, e.target.value)}
-                                            />
-                                            {option.label}
-                                        </label>
-                                    ))}
-                                </div>
-                            );
-                        }
-                        return null;
-                    })}
-                    <button type="submit" className="form-submit-button">
-                        {form.submitLabel}
-                    </button>
-                </form>
+                {/* Intro Section */}
+                <div className="footer-intro inline-block">
+                    <h2>{intro.title}</h2>
+                    <div
+                        className="footer-intro-text"
+                        dangerouslySetInnerHTML={{ __html: intro.text }}
+                    />
+                    <Link to={intro.link.href} className="footer-link " style={{ color: '#FF9900' }}>
+                        {intro.link.label}
+                        <ArrowRight className='inline ml-1' fill='orange' />
+                    </Link>
+                </div>
+
+                {/* Form Section */}
+                <div className="footer-form inline-block">
+                    <h3>{form.title}</h3>
+                    <form name={form.name} onSubmit={handleSubmit}>
+                        {/* <ArrowSelect /> */}
+                        {form.fields.map((field, index) => {
+                            if (field.type === 'text') {
+                                return (
+                                    <input
+                                        key={index}
+                                        type="text"
+                                        name={field.name}
+                                        placeholder={field.name}
+                                        className={`form-input ${field.size === 'half' ? 'half-width' : 'full-width'}`}
+                                        onChange={(e) => handleChange(field.name, e.target.value)}
+                                    />
+                                );
+                            } else if (field.type === 'textarea') {
+                                return (
+                                    <textarea
+                                        key={index}
+                                        name={field.name}
+                                        placeholder={field.name}
+                                        className={`form-textarea ${field.size === 'full' ? 'full-width' : ''}`}
+                                        onChange={(e) => handleChange(field.name, e.target.value)}
+                                    />
+                                );
+                            } else if (field.type === 'options') {
+                                return field.layout === 'select' ? (
+                                    <select
+                                        key={index}
+                                        name={field.name}
+                                        className={`form-select ${field.size === 'half' ? 'half-width' : 'full-width'}`}
+                                        onChange={(e) => handleChange(field.name, e.target.value)}
+                                    >
+                                        {field.options.map((option, i) => (
+                                            <option key={i} value={option.value}>
+                                                {option.label}
+                                            </option>
+                                        ))}
+                                    </select>
+                                ) : (
+                                    <div key={index} className={`form-buttons radio-toolbar ${field.size === 'full' ? 'full-width' : 'half-width'}`}>
+                                        {field.options.map((option, i) => (
+                                            <label key={i} className="form-button-option">
+                                                <input
+                                                    type="radio"
+                                                    name={field.name}
+                                                    value={option.value}
+                                                    onChange={(e) => handleChange(field.name, e.target.value)}
+                                                />
+                                                {option.label}
+                                            </label>
+                                        ))}
+                                    </div>
+                                );
+                            }
+                            return null;
+                        })}
+                        <div className="hero-link inline">
+                        </div>
+                        <Button label={form.submitLabel} type="submit" className="form-submit-button mt-2 primary" color='#FF9900' hoverColor='#ED8E00' />
+                    </form>
+                </div>
             </div>
         </div>
     );
